e429f4166bf93c9e3d52411d2b73ceed38c6c038
AzzahraFebia <azafebiascd@gmail.com>
2025-08-30 18:45:26 +0700

    Add files via upload

---
+++ Zara_project1.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.zara_project1;
+
+/**
+ *
+ * @author zara
+ */
+public class Zara_project1 {
+
+    public static void main(String[] args) {
+        System.out.println("Nama   : Azzahra");
+        System.out.println("NPM    : 2420506023");
+        System.out.println("Alamat : Mungkid, Magelang");
+        System.out.println("No_Hp  : 11223344566");
+    }
+}
